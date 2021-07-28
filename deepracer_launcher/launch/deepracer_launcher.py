6b0244438c39aed8e77c1d73553f2457c89c52f3
Pratik <pnichat@amazon.com>
2021-07-28 15:13:06 -0700

    Update launch script with deepracer_systems_scripts_node

--- deepracer_launcher/launch/deepracer_launcher.py
+++ deepracer_launcher/launch/deepracer_launcher.py
@@ -65,6 +65,12 @@ def generate_launch_description():
         executable='network_monitor_node',
         name='network_monitor_node'
     )
+    deepracer_systems_scripts_node = Node(
+        package='deepracer_systems_pkg',
+        namespace='deepracer_systems_pkg',
+        executable='deepracer_systems_scripts_node',
+        name='deepracer_systems_scripts_node'
+    )
     device_info_node = Node(
         package='device_info_pkg',
         namespace='device_info_pkg',
@@ -145,6 +151,7 @@ def generate_launch_description():
     ld.add_action(model_loader_node)
     ld.add_action(otg_control_node)
     ld.add_action(network_monitor_node)
+    ld.add_action(deepracer_systems_scripts_node)
     ld.add_action(device_info_node)
     ld.add_action(battery_node)
     ld.add_action(inference_node)
